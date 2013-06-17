4aa61de11727cd5c75cfbf19dec7ef0a08e0aefa
Tal Lev-Ami <tal.levami@gmail.com>
2013-06-17 17:15:10 +0300

    Increment version to v1.0.5: Fix handling of boolean params in upload

--- Cloudinary/CLCloudinary.m
+++ Cloudinary/CLCloudinary.m
@@ -12,7 +12,7 @@
 #import "NSString+CLURLEncoding.h"
 #import "NSDictionary+CLUtilities.h"
 
-NSString * const CL_VERSION = @"1.0.4";
+NSString * const CL_VERSION = @"1.0.5";
 
 NSString * const CL_CF_SHARED_CDN = @"d3jpl91pxevbkh.cloudfront.net";
 NSString * const CL_AKAMAI_SHARED_CDN = @"cloudinary-a.akamaihd.net";
