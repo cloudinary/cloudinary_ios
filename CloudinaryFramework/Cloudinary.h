f3cb7fc8fec2dbab12020161f484284d5dfa5205
Alexander Kolov <me@alexkolov.com>
2015-04-23 17:41:20 +0200

    Framework target

---
+++ CloudinaryFramework/Cloudinary.h
@@ -0,0 +1,20 @@
+//
+//  Cloudinary.h
+//  Cloudinary
+//
+//  Created by Alexander Kolov on 23.4.15.
+//  Copyright (c) 2015 Cloudinary Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CloudinaryFramework.
+FOUNDATION_EXPORT double CloudinaryFrameworkVersionNumber;
+
+//! Project version string for CloudinaryFramework.
+FOUNDATION_EXPORT const unsigned char CloudinaryFrameworkVersionString[];
+
+#import <Cloudinary/CLCloudinary.h>
+#import <Cloudinary/CLTransformation.h>
+#import <Cloudinary/CLEagerTransformation.h>
+#import <Cloudinary/CLUploader.h>
---
+++ CloudinaryFrameworkTests/CloudinaryFrameworkTests.m
@@ -0,0 +1,40 @@
+//
+//  CloudinaryFrameworkTests.m
+//  CloudinaryFrameworkTests
+//
+//  Created by Alexander Kolov on 23.4.15.
+//  Copyright (c) 2015 Cloudinary Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CloudinaryFrameworkTests : XCTestCase
+
+@end
+
+@implementation CloudinaryFrameworkTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
