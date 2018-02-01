7aac32c3d47255e720f78038f454345f95698aa7
Nitzan Jaitman <nitzan@cloudinary.com>
2018-02-01 11:29:43 +0200

    Addresponsive url generation tests.

--- CloudinaryTests/UIExtensions/UIImageViewTests.swift
+++ CloudinaryTests/UIExtensions/UIImageViewTests.swift
@@ -49,7 +49,7 @@ class UIImageViewTests: UIBaseTest {
     }
     
     func testResponsiveImageDownloadedAndSetFromURL() {
-        var expectation = self.expectation(description: "Upload should succeed")
+        let expectation = self.expectation(description: "Upload should succeed")
         
         var publicId: String?
         uploadFile().response({ (result, error) in
@@ -64,34 +64,28 @@ class UIImageViewTests: UIBaseTest {
             return
         }
         
-        expectation = self.expectation(description: "should succeed downloading and setting image.")
+        let dpr = UIScreen.main.scale
         
-        var imageDownloadedAndSet = false
-        let imageView = TestImageView { (image) in
-            if image != nil && image!.size.width == 450 && image!.size.height == 600 {
-                imageDownloadedAndSet = true
-            }
-            expectation.fulfill()
-        }
+        // Test auto_fill
+        // **************
+        var params = CLDResponsiveParams.autoFill().setStepSize(50).setMaxDimension(350).setMinDimension(100)
+        validateResponsiveGeneration(publicId: pubId, params: params, viewWidth: 145, viewHeight: 172, expectedImageWidth: 150 * dpr, expectedImageHeight: 200 * dpr, label: "fill, step size rounding")
+        validateResponsiveGeneration(publicId: pubId, params: params, viewWidth: 210, viewHeight: 142, expectedImageWidth: 250 * dpr, expectedImageHeight: 150 * dpr, label: "fill, step size rounding")
+        validateResponsiveGeneration(publicId: pubId, params: params, viewWidth: 100, viewHeight: 100, expectedImageWidth: 100 * dpr, expectedImageHeight: 100 * dpr, label: "fill, step size rounding, equal to min")
+        validateResponsiveGeneration(publicId: pubId, params: params, viewWidth: 10, viewHeight: 10, expectedImageWidth: 100 * dpr, expectedImageHeight: 100 * dpr, label: "fill, step size rounding, below minimum")
+        validateResponsiveGeneration(publicId: pubId, params: params, viewWidth: 10000, viewHeight: 10000, expectedImageWidth: 350 * dpr, expectedImageHeight: 350 * dpr, label: "fill, step size rounding, above maximum")
         
-        let params = CLDResponsiveParams(autoWidth: true, autoHeight: true,
-                                         cropMode: CLDTransformation.CLDCrop.fill,
-                                         gravity: CLDTransformation.CLDGravity.faces)
-            .setStepSize(50)
-        
-        imageView.frame = CGRect(x: 0, y: 0, width: 145, height: 172)
-        imageView.layoutMargins.bottom = 0
-        imageView.layoutMargins.top = 0
-        imageView.layoutMargins.left = 0
-        imageView.layoutMargins.right = 0
-        
-        imageView.cldSetImage(publicId: pubId, cloudinary: cloudinary!, responsiveParams: params)
-        imageView.layoutSubviews()
-
-        waitForExpectations(timeout: timeout, handler: nil)
-        
-        XCTAssertTrue(imageDownloadedAndSet)
-
+        // Test fit
+        // ********
+        params = CLDResponsiveParams.fit().setStepSize(50).setMaxDimension(350).setMinDimension(100)
+        validateResponsiveGeneration(publicId: pubId, params: params, viewWidth: 100, viewHeight: 300, expectedImageWidth: 100 * dpr, expectedImageHeight: 100 * dpr, label: "fit, step size rounding, equal to min")
+        validateResponsiveGeneration(publicId: pubId, params: params, viewWidth: 210, viewHeight: 190, expectedImageWidth: 200 * dpr, expectedImageHeight: 200 * dpr, label: "fit, step size rounding")
+        validateResponsiveGeneration(publicId: pubId, params: params, viewWidth: 300, viewHeight: 100, expectedImageWidth: 100 * dpr, expectedImageHeight: 100 * dpr, label: "fit, step size rounding, equal to min")
+        validateResponsiveGeneration(publicId: pubId, params: params, viewWidth: 1, viewHeight: 1000, expectedImageWidth: 100 * dpr, expectedImageHeight: 100 * dpr, label: "fit, step size rounding, below min")
+        validateResponsiveGeneration(publicId: pubId, params: params, viewWidth: 1000, viewHeight: 1, expectedImageWidth: 100 * dpr, expectedImageHeight: 100 * dpr, label: "fit, step size rounding, below min")
+        validateResponsiveGeneration(publicId: pubId, params: params, viewWidth: 200, viewHeight: 10000, expectedImageWidth: 200 * dpr, expectedImageHeight: 200 * dpr, label: "fit, step size rounding, above max")
+        validateResponsiveGeneration(publicId: pubId, params: params, viewWidth: 20000, viewHeight: 200, expectedImageWidth: 200 * dpr, expectedImageHeight: 200 * dpr, label: "fit, step size rounding, above max")
+        validateResponsiveGeneration(publicId: pubId, params: params, viewWidth: 200, viewHeight: 200, expectedImageWidth: 200 * dpr, expectedImageHeight: 200 * dpr, label: "fit, step size rounding, equal to step size")
     }
     
     func testImageDownloadedAndSetFromPublicID() {
@@ -148,5 +142,28 @@ class UIImageViewTests: UIBaseTest {
         required init?(coder aDecoder: NSCoder) {
             fatalError("init(coder:) has not been implemented")
         }        
+    }
+    
+    private func validateResponsiveGeneration(publicId: String, params: CLDResponsiveParams, viewWidth: Int, viewHeight: Int, expectedImageWidth: CGFloat, expectedImageHeight: CGFloat, label: String) {
+        let expectation = self.expectation(description: "Should succeed fetching image with correct dimensions for \(label)")
+
+        var imageDownloadedAndSet = false
+        let imageView = TestImageView { (image) in
+            if image != nil && image!.size.width == expectedImageWidth && image!.size.height == expectedImageHeight {
+                imageDownloadedAndSet = true
+            }
+            expectation.fulfill()
+        }
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: viewWidth, height: viewHeight)
+        imageView.layoutMargins.bottom = 0
+        imageView.layoutMargins.top = 0
+        imageView.layoutMargins.left = 0
+        imageView.layoutMargins.right = 0
+        imageView.layoutSubviews()
+        imageView.cldSetImage(publicId: publicId, cloudinary: cloudinary!, responsiveParams: params)
+
+        waitForExpectations(timeout: timeout, handler: nil)
+        XCTAssertTrue(imageDownloadedAndSet)
     }
 }
