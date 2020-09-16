6eb2eb17dcb8d89f90a23fba45f52c3a2977b6d8
OzdGini <ozd@gini-apps.com>
2020-09-16 12:28:28 +0300

    feature/WIP_widget_editViewController - add EditViewController logic and tests

--- Cloudinary/Classes/Core/Features/UploadWidget/WidgetImages/DoneIconInstructions.swift
+++ Cloudinary/Classes/Core/Features/UploadWidget/WidgetImages/DoneIconInstructions.swift
@@ -25,29 +25,41 @@
 import UIKit
 
 class DoneIconInstructions : CLDImageDrawingInstructions {
-      
-    var targetSize : CGSize
-    var fillColor : UIColor
     
+    var targetSize         : CGSize
+    var fillColor          : UIColor
+    var ovalBackgroundColor: UIColor
+        
     // MARK: - Initialization
-    init(targetSize size: CGSize = CGSize(width: 24.0, height: 24.0), fillColor: UIColor = .white) {
-        self.targetSize = size
-        self.fillColor = fillColor
+    init(targetSize size: CGSize = CGSize(width: 60.0, height: 60.0), fillColor: UIColor = .white, ovalBackgroundColor: UIColor = UIColor(red: 38.0/255, green: 91.0/255, blue: 220.0/255, alpha: 1.0)) {
+        self.targetSize          = size
+        self.fillColor           = fillColor
+        self.ovalBackgroundColor = ovalBackgroundColor
     }
     
     // MARK: - draw
     func draw(in context: CGContext) {
         
-        let container = CGRect(origin: .zero, size: targetSize)
+        let container  = CGRect(origin: .zero, size: targetSize)
+        let vContainer = CGRect(origin: .zero, size: CGSize(width: targetSize.width/2, height: targetSize.height/2))
         
+        // This non-generic function dramatically improves compilation times of complex expressions.
+        func fastFloor(_ x: CGFloat) -> CGFloat { return floor(x) }
+
+        // Oval Drawing
+        let ovalPath = UIBezierPath(ovalIn: CGRect(x: container.minX + fastFloor(container.width * 0.00000 + 0.5), y: container.minY + fastFloor(container.height * 0.00000 + 0.5), width: fastFloor(container.width * 1.00000 + 0.5) - fastFloor(container.width * 0.00000 + 0.5), height: fastFloor(container.height * 1.00000 + 0.5) - fastFloor(container.height * 0.00000 + 0.5)))
+        ovalBackgroundColor.setFill()
+        ovalPath.fill()
+        
+        //// pathDone Drawing
         let pathDonePath = UIBezierPath()
-        pathDonePath.move(to: CGPoint(x: container.minX + 0.37500 * container.width, y: container.minY + 0.67500 * container.height))
-        pathDonePath.addLine(to: CGPoint(x: container.minX + 0.20000 * container.width, y: container.minY + 0.50000 * container.height))
-        pathDonePath.addLine(to: CGPoint(x: container.minX + 0.14167 * container.width, y: container.minY + 0.55833 * container.height))
-        pathDonePath.addLine(to: CGPoint(x: container.minX + 0.37500 * container.width, y: container.minY + 0.79167 * container.height))
-        pathDonePath.addLine(to: CGPoint(x: container.minX + 0.87500 * container.width, y: container.minY + 0.29167 * container.height))
-        pathDonePath.addLine(to: CGPoint(x: container.minX + 0.81667 * container.width, y: container.minY + 0.23333 * container.height))
-        pathDonePath.addLine(to: CGPoint(x: container.minX + 0.37500 * container.width, y: container.minY + 0.67500 * container.height))
+        pathDonePath.move(to: CGPoint(x: vContainer.midX + 0.37500 * vContainer.width, y: vContainer.midY + 0.67500 * vContainer.height))
+        pathDonePath.addLine(to: CGPoint(x: vContainer.midX + 0.20000 * vContainer.width, y: vContainer.midY + 0.50000 * vContainer.height))
+        pathDonePath.addLine(to: CGPoint(x: vContainer.midX + 0.14167 * vContainer.width, y: vContainer.midY + 0.55833 * vContainer.height))
+        pathDonePath.addLine(to: CGPoint(x: vContainer.midX + 0.37500 * vContainer.width, y: vContainer.midY + 0.79167 * vContainer.height))
+        pathDonePath.addLine(to: CGPoint(x: vContainer.midX + 0.87500 * vContainer.width, y: vContainer.midY + 0.29167 * vContainer.height))
+        pathDonePath.addLine(to: CGPoint(x: vContainer.midX + 0.81667 * vContainer.width, y: vContainer.midY + 0.23333 * vContainer.height))
+        pathDonePath.addLine(to: CGPoint(x: vContainer.midX + 0.37500 * vContainer.width, y: vContainer.midY + 0.67500 * vContainer.height))
         pathDonePath.close()
         pathDonePath.usesEvenOddFillRule = true
         fillColor.setFill()
