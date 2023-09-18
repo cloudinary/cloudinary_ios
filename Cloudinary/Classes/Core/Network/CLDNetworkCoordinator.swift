13ec701efe8667963faaab9477668d1c56205e7d
adimiz1 <adi.mizrahi@cloudinary.com>
2023-09-18 12:13:51 +0300

    Version 4.1.1

--- Cloudinary/Classes/Core/Network/CLDNetworkCoordinator.swift
+++ Cloudinary/Classes/Core/Network/CLDNetworkCoordinator.swift
@@ -27,7 +27,7 @@ import UIKit
 
 internal class CLDNetworkCoordinator: NSObject {
 
-    static let DEFAULT_VERSION =        "4.1.0"
+    static let DEFAULT_VERSION =        "4.1.1"
     
     fileprivate struct CLDNetworkCoordinatorConsts {
         static let BASE_CLOUDINARY_URL =    "https://api.cloudinary.com"
