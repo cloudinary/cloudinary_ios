1ba61339c5e7278fa462d3dbd99451bb6436b506
adimiz1 <adi.mizrahi@cloudinary.com>
2023-12-18 16:23:22 +0200

    Add video analytics

---
+++ Cloudinary/Classes/ios/Video/Analytics/VideoEventsManager.swift
@@ -0,0 +1,121 @@
+//
+//  VideoEventsManager.swift
+//  Cloudinary
+//
+//  Created by Adi Mizrahi on 14/12/2023.
+//
+
+import Foundation
+@objcMembers public class VideoEventsManager {
+
+    let CLD_ANALYTICS_ENDPOINT_PRODUCTION_URL: String = "https://video-analytics-api.cloudinary.com/v1/video-analytics"
+    public var CLD_ANALYTICS_ENDPOINT_DEVELOPMENT_URL = "http://localhost:3001/events"
+
+    var viewId: String
+
+    var trackingType: TrackingType = .auto
+
+    var cloudName: String?
+
+    var publicId: String?
+
+    public init() {
+        viewId = UUID().uuidString.lowercased().replacingOccurrences(of: "-", with: "")
+    }
+
+    public func sendViewStartEvent(videoUrl: String, providedData: [String: Any]? = nil) {
+        let event = VideoViewStartEvent(trackingType: trackingType, viewId: viewId, videoUrl: videoUrl, trackingData: ["cloudName": cloudName ?? "", "publicId": publicId ?? ""], providedData: providedData)
+        addEventToQueue(event: event)
+    }
+
+    public func sendViewEndEvent(providedData: [String: Any]? = nil) {
+        let event = VideoViewEnd(trackingType: trackingType, viewId: viewId,  providedData: providedData)
+        addEventToQueue(event: event)
+    }
+
+    public func sendLoadMetadataEvent(duration: Int, providedData: [String: Any]? = nil) {
+        let event = VideoLoadMetadata(trackingType: trackingType, viewId: viewId, duration: duration, providedData: providedData)
+        addEventToQueue(event: event)
+    }
+
+    public func sendPlayEvent(providedData: [String: Any]? = nil) {
+        let event = VideoPlayEvent(trackingType: trackingType, viewId: viewId, providedData: providedData)
+        addEventToQueue(event: event)
+
+    }
+
+    public func sendPauseEvent(providedData: [String: Any]? = nil) {
+        let event = VideoPauseEvent(trackingType: trackingType, viewId: viewId, providedData: providedData)
+        addEventToQueue(event: event)
+    }
+
+    public var eventQueue = [VideoEvent]()
+
+    func addEventToQueue(event: VideoEvent) {
+        eventQueue.append(event)
+    }
+
+    @objc public func sendEvents() {
+        guard !eventQueue.isEmpty else {
+            print("Event queue is empty")
+            return
+        }
+
+        let eventsToSend = Array(eventQueue.prefix(eventQueue.count))
+        sendEventToEndpoint(childEvents: eventsToSend)
+        eventQueue.removeFirst(eventsToSend.count)
+    }
+
+    private func sendEventToEndpoint(childEvents: [VideoEvent]) {
+        var request = URLRequest(url: URL(string: CLD_ANALYTICS_ENDPOINT_PRODUCTION_URL)!)
+        request.httpMethod = "POST"
+
+        let boundary = "Boundary-\(UUID().uuidString)"
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        var body = Data()
+
+        // Add events data
+
+        body.append(contentsOf: "--\(boundary)\r\n".utf8)
+        body.append(contentsOf: "Content-Disposition: form-data; name=\"userId\"\r\n\r\n".utf8)
+        body.append(contentsOf: "\(childEvents[0].userId!)\r\n".utf8)
+
+        body.append(contentsOf: "--\(boundary)\r\n".utf8)
+        body.append(contentsOf: "Content-Disposition: form-data; name=\"viewId\"\r\n\r\n".utf8)
+        body.append(contentsOf: "\(childEvents[0].viewId)\r\n".utf8)
+
+        body.append(contentsOf: "--\(boundary)\r\n".utf8)
+        body.append(contentsOf: "Content-Disposition: form-data; name=\"events\"\r\n\r\n".utf8)
+        var eventData: [[String: Any]] = []
+        for childEvent in childEvents {
+            print("Sending event: \(childEvent.eventName)")
+            eventData.append([
+                VideoEventJSONKeys.eventName.rawValue: childEvent.eventName,
+                VideoEventJSONKeys.eventTime.rawValue: childEvent.eventTime,
+                VideoEventJSONKeys.eventDetails.rawValue: childEvent.eventDetails])
+        }
+
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: eventData, options: [])
+            body.append(jsonData)
+            body.append(contentsOf: "\r\n".utf8)
+        } catch {
+            print("Error creating JSON data: \(error)")
+        }
+
+
+        body.append(contentsOf: "--\(boundary)--\r\n".utf8)
+        request.httpBody = body
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error sending event: \(error)")
+            } else if let httpResponse = response as? HTTPURLResponse,
+                      (200...299).contains(httpResponse.statusCode) {
+                //Success
+            } else {
+                print("Failed to send event. Response: \(response.debugDescription)")
+            }
+        }.resume()
+    }
+}
---
+++ Example/Tests/UIExtensions/Video Analytics/VideoEventsTests.swift
@@ -0,0 +1,91 @@
+//
+//  VideoEventsTests.swift
+//  Cloudinary_Tests
+//
+//  Created by Adi Mizrahi on 18/12/2023.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import Cloudinary
+public class VideoEventTests: XCTestCase {
+
+    func testVideoViewStartEventInitialization() {
+        let viewId = "123456"
+        let videoUrl = "https://www.example.com/video.mp4"
+        let trackingData = ["cloudName": "exampleCloud", "publicId": "abc123"]
+        let providedData = ["key": "value"]
+
+        let event = VideoViewStartEvent(viewId: viewId, videoUrl: videoUrl, trackingData: trackingData, providedData: providedData)
+
+        XCTAssertEqual(event.viewId, viewId)
+        XCTAssertEqual(event.eventName, EventNames.viewStart.rawValue)
+        XCTAssertEqual(event.eventDetails[VideoEventJSONKeys.trackingType.rawValue] as? String, TrackingType.auto.rawValue)
+        XCTAssertEqual(event.eventDetails[VideoEventJSONKeys.videoUrl.rawValue] as? String, videoUrl)
+
+        if let customerData = event.eventDetails[VideoEventJSONKeys.customerData.rawValue] as? [String: Any],
+           let videoData = customerData[VideoEventJSONKeys.videoData.rawValue] as? [String: Any] {
+            XCTAssertEqual(videoData[VideoEventJSONKeys.cloudName.rawValue] as? String, trackingData["cloudName"])
+            XCTAssertEqual(videoData[VideoEventJSONKeys.publicId.rawValue] as? String, trackingData["publicId"])
+        }
+
+        if let providedDataObject = event.eventDetails[VideoEventJSONKeys.providedData.rawValue] as? [String: Any] {
+            XCTAssertEqual(providedDataObject["key"] as? String, providedData["key"])
+        }
+    }
+
+    func testVideoLoadMetadataEventInitialization() {
+        let viewId = "123456"
+        let duration = 120
+
+        let event = VideoLoadMetadata(viewId: viewId, duration: duration)
+
+        XCTAssertEqual(event.viewId, viewId)
+        XCTAssertEqual(event.eventName, EventNames.loadMetadata.rawValue)
+        XCTAssertEqual(event.eventDetails[VideoEventJSONKeys.trackingType.rawValue] as? String, TrackingType.auto.rawValue)
+        XCTAssertEqual(event.eventDetails[VideoEventJSONKeys.videoDuration.rawValue] as? Int, duration)
+    }
+
+    func testVideoViewEndEventInitialization() {
+        let viewId = "123456"
+        let providedData = ["key": "value"]
+
+        let event = VideoViewEnd(viewId: viewId, providedData: providedData)
+
+        XCTAssertEqual(event.viewId, viewId)
+        XCTAssertEqual(event.eventName, EventNames.viewEnd.rawValue)
+        XCTAssertEqual(event.eventDetails[VideoEventJSONKeys.trackingType.rawValue] as? String, TrackingType.auto.rawValue)
+
+        if let providedDataObject = event.eventDetails[VideoEventJSONKeys.providedData.rawValue] as? [String: Any] {
+            XCTAssertEqual(providedDataObject["key"] as? String, providedData["key"])
+        }
+    }
+
+    func testVideoPlayEventInitialization() {
+        let viewId = "123456"
+        let providedData = ["key": "value"]
+
+        let event = VideoPlayEvent(viewId: viewId, providedData: providedData)
+
+        XCTAssertEqual(event.viewId, viewId)
+        XCTAssertEqual(event.eventName, EventNames.play.rawValue)
+
+        if let providedDataObject = event.eventDetails[VideoEventJSONKeys.providedData.rawValue] as? [String: Any] {
+            XCTAssertEqual(providedDataObject["key"] as? String, providedData["key"])
+        }
+    }
+
+    func testVideoPauseEventInitialization() {
+        let viewId = "123456"
+        let providedData = ["key": "value"]
+
+        let event = VideoPauseEvent(viewId: viewId, providedData: providedData)
+
+        XCTAssertEqual(event.viewId, viewId)
+        XCTAssertEqual(event.eventName, EventNames.pause.rawValue)
+
+        if let providedDataObject = event.eventDetails[VideoEventJSONKeys.providedData.rawValue] as? [String: Any] {
+            XCTAssertEqual(providedDataObject["key"] as? String, providedData["key"])
+        }
+    }
+}
